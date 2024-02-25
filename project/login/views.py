c3097392c351cd72afa649776e9ad8cdbdc13210
Diego Jacinto <diego.jacinto6119@alumnos.udg.mx>
2024-02-24 19:28:48 -0600

    Proyecto horacio

---
+++ project/login/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import User
+from django.shortcuts import redirect
+
+# Create your views here.
+def home(request):
+    return render(request, 'login.html')
+
+def register_user(request):
+    username= request.POST['username']
+    password= request.POST['password']
+    user = User(username=username, password=password)
+    user.save()
+    return redirect('/')
